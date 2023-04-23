20b193a3d9e5f03e94c1ca69c7cf4d3f45de38a8
Rajesh P <rajp.gr8@gmail.com>
2023-04-23 12:25:10 +0100

    Create 1.kt

---
+++ 1.kt
@@ -0,0 +1,43 @@
+fun main (args: Array<String>) {
+    println("Hello World!")
+    println("Program arguments: ${args.joinToString()}")
+
+    val age =  100
+    //age = 190  //Error: Kotlin: Val cannot be reassigned
+    println(age)
+
+    //String
+    val name = "Rajesh"
+    println("(Example of String): My name is $name")
+    println(name[0])
+    println("Length of name:" + name.length)
+    println(name.substring(2,4))
+
+    //Nullable value
+    //val remark: String? = null   //Nullable value
+    val remark: String? = "Abc"   //Nullable value
+    println(remark?.uppercase()) // OR can use if to check non-null value
+    if(remark != null) {
+        println(remark.uppercase())
+    }
+
+
+    //Collection (mutable vs non-mutable)
+    val allNames = listOf("John", "Mahesh", "Smith")    //Non-mutable list (like Python tuple)
+    println("(Example of collections): $allNames")
+    println(allNames[2])
+
+    val allNames2 = mutableListOf("John", "Mahesh", "Smith") //Mutable list (like Python list)
+    allNames2.add("Ganesh");
+    //allNames2.add(89); //Error: Kotlin: The integer literal does not conform to the expected type String
+    println(allNames2)
+
+    //for loops
+    for(name in allNames2) {
+        println(name)
+    }
+
+    for(i in 1..4) {  //OR for (i in 1..4) {} OR for (i in until 4) {}
+        println(i)
+    }
+}
